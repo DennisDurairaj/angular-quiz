333b10a4f75e836bb844297fc6357577534a9f55
Dennis Durairaj <dennis.durairaj@gmail.com>
2016-07-26 14:38:07 +0200

    Added views for the quiz questions

--- js/controllers/quizCtrl.js
+++ js/controllers/quizCtrl.js
@@ -4,5 +4,17 @@
 		var self = this;
 		self.quizObj = quizService;
 		self.dataObj = dataService;
+		self.activeQuestion = 0;
+		self.questionAnswered = function () {
+
+		};
+
+		dataService.getQuestions().then(function (response) {
+			self.questions = response.data;
+		});
+
+		self.getActiveQuestion = function () {
+
+		}
 	}]);
 })();
